7a5b6a49baa4ed9666fdcfd12f73e498b2eb2368
MathiasHeimbach <60534997+MathiasHeimbach@users.noreply.github.com>
2023-02-17 12:43:43 +0100

    add movable element and Schweif

--- index.ts
+++ index.ts
@@ -18,6 +18,97 @@ timer(0, 1000)
   .pipe(map(() => new Date().toLocaleTimeString()))
   .subscribe(time => (output.textContent = time));
 
+// Movable Element
+import { fromEvent, exhaustMap, takeUntil } from 'rxjs';
+
+const target = document.createElement('div');
+target.setAttribute(
+  'style',
+  'position: absolute; top: 0; left: 0; background-color: red; width: 50px; height: 50px;'
+);
+document.body.append(target);
+
+fromEvent(target, 'mousedown')
+  .pipe(
+    exhaustMap(() =>
+      fromEvent(document, 'mousemove').pipe(
+        takeUntil(fromEvent(document, 'mouseup'))
+      )
+    )
+  )
+  .subscribe(({ pageX, pageY }: MouseEvent) => {
+    target.style.transform = `translate3d(${pageX}px, ${pageY}px, 0)`;
+  });
+
+
+// Animated Dot Trail
+
+import {
+  defer,
+  animationFrames,
+  mergeMap,
+  takeWhile,
+  finalize
+} from 'rxjs';
+
+// When the mouse moves, add animated dots to the screen.
+fromEvent(document, 'mousemove')
+  .pipe(mergeMap((e: MouseEvent) => addDot(e.pageX, e.pageY)))
+  .subscribe();
+
+function addDot(x: number, y: number) {
+  return defer(() => {
+    // Create and add the dot element when
+    // the observable is subscribed to
+    const dot = document.createElement('div');
+    dot.setAttribute(
+      'style',
+      `
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 10px;
+        height: 10px;
+        background-color: lime;
+        border-radius: 50%;
+        transform: translate3d(${x}px, ${y}px, 0);
+      `
+    );
+    document.body.append(dot);
+
+    const xVelocity = Math.random() * 2 - 1;
+    const yVelocity = Math.random() * 2 - 1;
+
+    return animationFrames().pipe(
+      // Only take animation frames for 1 second.
+      takeWhile(({ elapsed }) => elapsed < 1000),
+
+      // Track and update the current position.
+      scan(
+        ({ x: xCurrent, y: yCurrent }) => ({
+          x: xCurrent + xVelocity,
+          y: yCurrent + yVelocity
+        }),
+        { x, y }
+      ),
+
+      // Set the position on the dot as a side-effect.
+      tap(({ x, y }) => {
+        dot.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+      }),
+
+      // When we clean up, remove the element.
+      finalize(() => {
+        dot.remove();
+      })
+    );
+  });
+}
+      
+      
+
+
+
 // obsExample() {
     const obs1= interval(1000)
       .pipe(
@@ -63,3 +154,4 @@ timer(0, 1000)
   
       obs4.subscribe(value => console.log("observerShare 2 received " + value));
 //    }
+
