9f6c4dff6dcd48db3fbf29cace4daf350f3269a5
MathiasHeimbach <60534997+MathiasHeimbach@users.noreply.github.com>
2023-02-22 09:16:32 +0100

    addObservableExample

--- index.ts
+++ index.ts
@@ -154,3 +154,27 @@ function addDot(x: number, y: number) {
       obs4.subscribe(value => console.log("observerShare 2 received " + value));
 //    }
 
+// Create simple observable that emits three values
+const myObservable = of(1, 2, 3);
+
+// Create observer object
+const myObserver = {
+  next: (x: number) => console.log('Observer got a next value: ' + x),
+  error: (err: Error) => console.error('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification'),
+};
+
+// Execute with the observer object
+myObservable.subscribe(myObserver);
+
+// Logs:
+// Observer got a next value: 1
+// Observer got a next value: 2
+// Observer got a next value: 3
+// Observer got a complete notification
+
+myObservable.subscribe(
+  x => console.log('Observer got a next value: ' + x),
+  err => console.error('Observer got an error: ' + err),
+  () => console.log('Observer got a complete notification')
+);
